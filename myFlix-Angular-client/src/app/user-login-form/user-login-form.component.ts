d5d0651fba133b72ff702fc45e719005830f2a60
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-09-09 19:16:59 -0600

    6.3 - Added User Reg and Log

--- myFlix-Angular-client/src/app/user-login-form/user-login-form.component.ts
+++ myFlix-Angular-client/src/app/user-login-form/user-login-form.component.ts
@@ -11,34 +11,35 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
-  selector: 'app-user-registration-form',
-  templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss']
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
 })
-export class UserRegistrationFormComponent implements OnInit {
+export class UserLoginFormComponent implements OnInit {
 
-  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+  @Input() loginData = { Username: '', Password: '' };
 
   constructor(
     public fetchApiData: FetchApiDataService,
-    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
   // This is the function responsible for sending the form inputs to the backend
-  registerUser(): void {
-    this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
-      // Logic for a successful user registration goes here! (To be implemented)
-      console.log(response)
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.loginData).subscribe((result) => {
+      // Logic for a successful user login goes here! (To be implemented)
+      console.log(result);
+      localStorage.setItem('username', result.user.Username);
+      localStorage.setItem('token', result.token);
       this.dialogRef.close(); // This will close the modal on success!
-      this.snackBar.open('User successfully registered', 'OK', {
+      this.snackBar.open('Logged in', 'OK', {
         duration: 2000
       });
-    }, (response) => {
-      console.log(response)
-      this.snackBar.open(response, 'OK', {
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
         duration: 2000
       });
     });
